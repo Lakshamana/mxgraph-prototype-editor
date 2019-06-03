3f5e8d96e4c8548a50d128ff7d9fc7c0ab1d89e8
Lakshamana <guitrompa1@gmail.com>
2019-06-03 13:33:26 -0300

    Adiciona parser para XML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-const mxgraph = require('mxgraph')({
-  mxImageBasePath: 'src/images',
-  mxBasePath: 'src'
-})
 const fs = require('fs')
+const mxgraph = require('mxgraph')({
+  mxImageBasePath: "./src/images",
+  mxBasePath: "./src"
+})
+
+function parseXmlToGraph(xmlDoc, graph) {
+  const cells = xmlDoc.documentElement.children[0].children
+  const parent = graph.getDefaultParent()
+  for (let i = 0; i < cells.length; i++) {
+    const cellAttrs = cells[i].attributes
+    if(cellAttrs.vertex) {
+      const vertexName = cellAttrs.value.value
+      const vertexId = Number(cellAttrs.id.value) 
+      const geom = cells[i].children[0].attributes
+      const xPos = Number(geom.x.value)
+      const yPos = Number(geom.y.value)
+      const height = Number(geom.height.value)
+      const width = Number(geom.width.value)
+      console.log({
+        vertexId, vertexName, xPos, yPos, width, height
+      })
+      graph.insertVertex(parent, vertexId, vertexName, xPos, yPos, width, height)
+    } else if(cellAttrs.edge) {
+      const edgeName = cellAttrs.value.value
+      const edgeId = Number(cellAttrs.id.value)
+      const source = Number(cellAttrs.source.value)
+      const target = Number(cellAttrs.target.value)
+      console.log({
+        edgeId, edgeName, source, target
+      })
+      graph.insertEdge(parent, edgeId, edgeName,
+        graph.getModel().getCell(source), 
+        graph.getModel().getCell(target)
+      )
+    }
+  }
+}
 
 function main(container) {
   // Checks if the browser is supported
-  if (!mxgraph.mxClient.isBrowserSupported()) {
+  if (!mxgraph.mxClient.isBrowserSupported()) { 
     // Displays an error message if the browser is not supported.
     mxgraph.mxUtils.error('Browser is not supported!', 200, false)
   } else {
@@ -14,23 +47,20 @@ function main(container) {
     mxgraph.mxEvent.disableContextMenu(container)
 
     // Creates the graph inside the given container
-    var graph = new mxgraph.mxGraph(container)
+    const graph = new mxgraph.mxGraph(container)
 
     //Adds a new HierarquicalLayout
-    var layout = new mxgraph.mxHierarchicalLayout(graph)
+    const layout = new mxgraph.mxHierarchicalLayout(graph)
     
-    // Adds cells to the model in a mxgraph prototype editor single step
+    // Adds cells to the model in a mxgraph.mxgraph prototype editor single step
     graph.getModel().beginUpdate()
     try {
       //deal with xml
-      var xmlstr = fs.readFileSync('src/resources/diag.xml', 'utf-8')
-      console.log(`%${xmlstr}%`)
-      var doc = mxgraph.mxUtils.parseXml(xmlstr)
-      var codec = new mxgraph.mxCodec(doc)
-      const x = codec.decode(doc.documentElement, graph.getModel())
-      console.log(x)
+      const xmlstr = fs.readFileSync('src/resources/diag.xml', 'utf-8')
+      const doc = mxgraph.mxUtils.parseXml(xmlstr)
+      parseXmlToGraph(doc, graph)
       
-      var parent = graph.getDefaultParent()
+      const parent = graph.getDefaultParent()
       //execute layout
       layout.execute(parent)
     } finally {
@@ -40,6 +70,4 @@ function main(container) {
   }
 }
 
-window.onload = function() {
-  main(document.getElementById('graphContainer'))
-}
+window.onload = main(document.getElementById('graphContainer'))
