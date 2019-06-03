07ac7897586d242817bb4bcade6bf38ac158e890
Lakshamana <guitrompa1@gmail.com>
2019-06-03 13:42:03 -0300

    Limpeza em index.js

--- src/index.js
+++ src/index.js
@@ -17,18 +17,12 @@ function parseXmlToGraph(xmlDoc, graph) {
       const yPos = Number(geom.y.value)
       const height = Number(geom.height.value)
       const width = Number(geom.width.value)
-      console.log({
-        vertexId, vertexName, xPos, yPos, width, height
-      })
       graph.insertVertex(parent, vertexId, vertexName, xPos, yPos, width, height)
     } else if(cellAttrs.edge) {
       const edgeName = cellAttrs.value.value
       const edgeId = Number(cellAttrs.id.value)
       const source = Number(cellAttrs.source.value)
       const target = Number(cellAttrs.target.value)
-      console.log({
-        edgeId, edgeName, source, target
-      })
       graph.insertEdge(parent, edgeId, edgeName,
         graph.getModel().getCell(source), 
         graph.getModel().getCell(target)
@@ -38,21 +32,12 @@ function parseXmlToGraph(xmlDoc, graph) {
 }
 
 function main(container) {
-  // Checks if the browser is supported
   if (!mxgraph.mxClient.isBrowserSupported()) { 
-    // Displays an error message if the browser is not supported.
     mxgraph.mxUtils.error('Browser is not supported!', 200, false)
   } else {
-    // Disables the built-in context menu
     mxgraph.mxEvent.disableContextMenu(container)
-
-    // Creates the graph inside the given container
     const graph = new mxgraph.mxGraph(container)
-
-    //Adds a new HierarquicalLayout
     const layout = new mxgraph.mxHierarchicalLayout(graph)
-    
-    // Adds cells to the model in a mxgraph.mxgraph prototype editor single step
     graph.getModel().beginUpdate()
     try {
       //deal with xml
@@ -64,7 +49,6 @@ function main(container) {
       //execute layout
       layout.execute(parent)
     } finally {
-      // Updates the display
       graph.getModel().endUpdate()
     }
   }
