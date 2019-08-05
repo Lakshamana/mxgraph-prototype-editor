7ed2d8cc3165559036cf672ac3da923a94ab6e60
Lakshamana <guitrompa1@gmail.com>
2019-08-05 16:50:18 -0300

    format max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'new-cap': 'off',
-    'space-before-function-paren': 'off'
+    'space-before-function-paren': 'off',
+    'max-len': 'error'
   },
   parserOptions: {
     parser: 'babel-eslint'
