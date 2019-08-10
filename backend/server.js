8cadcee2dd795d17f74069436955ec361e85c7ea
Lakshamana <guitrompa1@gmail.com>
2019-08-10 01:39:22 -0300

    add port number

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,12 @@ app.use(cors({ origin: `${HOST}` }))
 
 app.use(serve(path.resolve(__dirname, '../src/static/examples/editors')))
 
-// if (process.env.NODE_ENV === 'production') {
-app.use(serve(path.resolve(__dirname, '../dist')))
-app.use(async ctx => {
-  await send(ctx, path.resolve(__dirname, '../dist/index.html'))
-})
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(serve(path.resolve(__dirname, '../dist')))
+  app.use(async ctx => {
+    await send(ctx, path.resolve(__dirname, '../dist/index.html'))
+  })
+}
 
 const port = process.env.PORT || 3000
 app.listen(port)
