11469e308498c559635a437d19af38461297bbbc
Lakshamana <guitrompa1@gmail.com>
2019-08-10 00:14:12 -0300

    set cross-env as dep

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const serve = require('koa-static')
 const send = require('koa-send')
 const cors = require('@koa/cors')
 const config = require('./config')
+const path = require('path')
 
 const app = new koa()
 
@@ -12,14 +13,13 @@ app.use(cors({ origin: `${HOST}` }))
 
 app.use(serve('src/static/examples/editors'))
 
-let port = 3000
 if (process.env.NODE_ENV === 'production') {
-  app.use(serve('dist'))
+  app.use(serve('../dist'))
   app.use(async ctx => {
-    await send(ctx, '/dist/index.html')
+    await send(ctx, path.resolve(__dirname, '../dist/index.html'))
   })
-  port = 80
 }
 
+const port = process.env.PORT || 3000
 app.listen(port)
 console.log(`Server running on ${HOST}`)
